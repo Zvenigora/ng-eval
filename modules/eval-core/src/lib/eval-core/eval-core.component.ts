136123f65c2ae9193649f19d529e902278bf2ffb
Mikhail Klygin <mklygin@goallinesolutions.com>
2023-11-26 11:59:30 -0500

    feat: added Acorn

--- modules/eval-core/src/lib/eval-core/eval-core.component.ts
+++ modules/eval-core/src/lib/eval-core/eval-core.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as acorn from "acorn";
+
+export function ngEval(): string {
+  console.log(acorn.parse("1 + 1", {ecmaVersion: 2020}));
+  return 'eval';
+}
+
 @Component({
   selector: 'zvenigora-eval-core',
   standalone: true,
@@ -8,4 +15,9 @@ import { CommonModule } from '@angular/common';
   templateUrl: './eval-core.component.html',
   styleUrl: './eval-core.component.scss',
 })
-export class EvalCoreComponent {}
+export class EvalCoreComponent {
+
+  constructor() {
+    console.log(acorn.parse("1 + 1", {ecmaVersion: 2020}));
+  }
+}
