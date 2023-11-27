03a45992180fc3b8d8469e5db29f8b6dd7479632
Mikhail Klygin <m.klygin@gmail.com>
2023-11-26 21:37:58 -0500

    feat: added Acorn declarations feat: added Parser service test: added tests for Parser service

--- modules/eval-core/src/lib/actual/services/parser.service.spec.ts
+++ modules/eval-core/src/lib/actual/services/parser.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ParserService } from './parser.service';
+import { BinaryExpression, ParserOptions } from '../../internal/interfaces';
 
 describe('ParserService', () => {
   let service: ParserService;
@@ -12,5 +13,51 @@ describe('ParserService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should parse expression', () => {
+    const expr = '1 + 2';
+    const ast = service.parse(expr, {} as ParserOptions);
+    expect(ast).toBeDefined();
+    expect(ast?.type).toBe('BinaryExpression');
+    expect((ast as BinaryExpression).operator).toBe('+');
+  });
+
+  it('should return version > 8', () => {
+    const version = service.version;
+    expect(parseFloat(version)).toBeGreaterThan(8);
+  });
+
+  it('#parse 1+ should return error', () => {
+    expect(() => service.parse('1+')).toThrow('Unexpected token (1:2)');
+  });
+
+  it('#parse [a] should return ArrayExpression', () => {
+    const ast = service.parse('[a]');
+    // const ast = {
+    //   type: 'ArrayExpression',
+    //   start: 0,
+    //   end: 3,
+    //   elements: [ { type: 'Identifier', start: 1, end: 2, name: 'a' } ]
+    // };
+
+    const expected = {
+      type: 'ArrayExpression',
+      elements: [{ type: 'Identifier', name: 'a' }]
+    };
+    expect(ast).toBeTruthy();
+    console.log(ast);
+    console.log(expected);
+
+    expect(ast).toEqual(
+      expect.objectContaining({
+        type: expected.type,
+        elements: expect.arrayContaining([
+          expect.objectContaining(
+            expected.elements[0]
+          )
+        ])
+      })
+    );
   });
 });
