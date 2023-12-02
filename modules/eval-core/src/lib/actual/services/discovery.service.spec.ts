25fe32e9b5dcaeff040c33f9067a3eb18b32fcb1
Mikhail Klygin <mklygin@goallinesolutions.com>
2023-12-02 11:04:38 -0500

    rename: discovery service to use findAll method

--- modules/eval-core/src/lib/actual/services/discovery.service.spec.ts
+++ modules/eval-core/src/lib/actual/services/discovery.service.spec.ts
@@ -1,49 +1,54 @@
 import { TestBed } from '@angular/core/testing';
 import { DiscoveryService } from './discovery.service';
+import { AnyNode, Identifier } from 'acorn';
+import { ParserService } from './parser.service';
 
 describe('DiscoveryService', () => {
   let service: DiscoveryService;
+  let parser: ParserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(DiscoveryService);
+    parser = TestBed.inject(ParserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should discover binary expressions in a program', () => {
-    const program = '1 + 2 * 3';
+  it('should find all binary expressions in a program', () => {
+    const program = '1 + 2 * a';
     const searchType = 'BinaryExpression';
-    const expressions = service.discover(program, searchType);
+    const expressions = service.findAll(program, searchType);
     expect(expressions).toBeDefined();
     expect(expressions?.length).toBe(2);
     expect(expressions?.[0].type).toBe('BinaryExpression');
   });
 
-  it('should discover identifier expressions in a program', () => {
-    const program = 'a + 2*b + c';
+  it('should find all identifier expressions in a program', () => {
+    const program = 'a + 2 * b + c';
     const searchType = 'Identifier';
-    const expressions = service.discover(program, searchType);
+    const expressions = service.findAll(program, searchType);
     expect(expressions).toBeDefined();
     expect(expressions?.length).toBe(3);
     expect(expressions?.[0].type).toBe('Identifier');
+    expect((expressions?.[0] as Identifier).name).toBe('a');
   });
 
-  // it('should discover expressions in an expression', () => {
-  //   const expression: { type: 'BinaryExpression', left: 1 , right: 2 };
-  //   const searchType = 'BinaryExpression';
-  //   const expressions = service.discover(expression, searchType);
-  //   expect(expressions).toBeDefined();
-  //   expect(expressions?.length).toBe(1);
-  //   expect(expressions?.[0].type).toBe('BinaryExpression');
-  // });
+  it('should find all binary expressions expressions in an expression', () => {
+    const expression: AnyNode | undefined = parser.parse('1 + 2 * a');
+    const searchType = 'BinaryExpression';
+    const expressions = service.findAll(expression, searchType);
+    expect(expressions).toBeDefined();
+    expect(expressions?.length).toBe(2);
+    expect(expressions?.[0].type).toBe('BinaryExpression');
+  });
 
   it('should return undefined if no expressions are found', () => {
     const program = '1 + 2';
     const searchType = 'Identifier';
-    const expressions = service.discover(program, searchType);
+    const expressions = service.findAll(program, searchType);
     expect(expressions).toBeUndefined();
   });
 
@@ -51,7 +56,8 @@ describe('DiscoveryService', () => {
     const program = '1 +';
     const searchType = 'BinaryExpression';
     expect(() => {
-      service.discover(program, searchType);
+      service.findAll(program, searchType);
     }).toThrow('Unexpected token (1:3)');
   });
+
 });
