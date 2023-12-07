b2cce4e9abdc37ee6c67ba657a39097d92eb0512
Mikhail Klygin <mklygin@goallinesolutions.com>
2023-12-06 22:08:54 -0500

    feat: started eval service

--- modules/eval-core/src/lib/actual/services/eval.service.spec.ts
+++ modules/eval-core/src/lib/actual/services/eval.service.spec.ts
@@ -13,4 +13,22 @@ describe('EvalService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should evaluate a string expression', () => {
+    const expression = '2 + 3 * a';
+    const context = { a: 10 };
+    const result = service.eval(expression, context);
+    expect(result).toEqual(32);
+  });
+
+  // it('should evaluate an AST expression', () => {
+  //   const expression = { type: 'BinaryExpression', operator: '+', left: 2, right: 2 };
+  //   const result = service.eval(expression);
+  //   expect(result).toEqual(4);
+  // });
+
+  // it('should throw an error if evaluation fails', () => {
+  //   const expression = '2 / 0';
+  //   expect(() => service.eval(expression)).toThrowError();
+  // });
 });
