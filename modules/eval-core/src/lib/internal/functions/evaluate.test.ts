645edb053a4ba271df2c13d3b4107187d25ffee8
Mikhail Klygin <m.klygin@gmail.com>
2023-12-26 20:46:06 -0500

    test: updated tests for parse and evaluate

--- modules/eval-core/src/lib/internal/functions/evaluate.test.ts
+++ modules/eval-core/src/lib/internal/functions/evaluate.test.ts
@@ -1,6 +1,7 @@
-import { AnyNode } from 'acorn';
+import { AnyNode, ecmaVersion } from 'acorn';
 import { EvalState } from '../classes/eval';
-import { evaluate } from './evaluate';
+import { evaluate, evaluateAsync } from './evaluate';
+import { parse } from './parse';
 
 
 describe('evaluate', () => {
@@ -26,6 +27,24 @@ describe('evaluate', () => {
     const state = EvalState.fromContext({});
     const result = evaluate(node, state);
     expect(result).toBeDefined(); // Replace with your expected value
+  });
+
+  it('#eval asyncFunc(one, two) should return 3', async () => {
+    const context = {
+      one: 1,
+      two: 2,
+      asyncFunc: async (a: number, b: number) => { return await (a+b); }
+    };
+
+    const options = { ecmaVersion: 2020 as ecmaVersion, extractExpressions: true };
+
+    const expr = 'asyncFunc(one, two)';
+    const node = parse(expr, options);
+    const state = EvalState.fromContext(context);
+
+    const value = await evaluateAsync(node, state);
+    expect(value).toBe(1 + 2);
+
   });
 
 });
